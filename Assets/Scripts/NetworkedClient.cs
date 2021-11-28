6fd64dfcb896347cede2a98bad8d04f0d17cc839
yungwym <wymer1120@gmail.com>
2021-11-28 01:57:37 -0500

    Commit - Opponent Turn Bug Fixes

--- Assets/Scripts/NetworkedClient.cs
+++ Assets/Scripts/NetworkedClient.cs
@@ -91,7 +91,7 @@ public class NetworkedClient : MonoBehaviour
             hostID = NetworkTransport.AddHost(topology, 0);
             Debug.Log("Socket open.  Host ID = " + hostID);
 
-            connectionID = NetworkTransport.Connect(hostID, "192.168.86.138", socketPort, 0, out error); // server is local on network
+            connectionID = NetworkTransport.Connect(hostID, "192.168.86.128", socketPort, 0, out error); // server is local on network
 
             if (error == 0)
             {
